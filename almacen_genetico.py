7875aef8788e2c6c7f8d49dc0435837e674ffb4f
Waaflee <agustinwafle@gmail.com>
2020-03-28 19:51:46 -0300

    Added crossover operator

--- almacen_genetico.py
+++ almacen_genetico.py
@@ -19,9 +19,38 @@ class GeneticStore():
     def populate(self):
         for i in range(reduce(lambda x, y: x*y*4, self.size)):
             self.population.append(generate_indexed_store(
-                self.size[0], self.size[1], randomize=True))
+                self.size[0], self.size[1], randomize=True)
+            )
 
-    def fitness(self, store):
+    def fitness(self):
+        stores = []
+        # compute scrores
+        for i in self.population:
+            stores.append({"score": self.score(i), "store": store})
+        max_score = max([i["score"] for i in stores])
+        return stores, max_score
+        # sort
+        # stores.sort(key=lambda x: x["score"], reverse=True)
+        # normalize
+        # normalized_score = [i/max(stores) for i in stores]
+
+    def match(self, stores, max_score):
+        weights = [1 - i["score"] / max_score for i in stores]
+        new_population = []
+        for i in len(self.population)/2:
+            match = random.choices(stores, weights=weights, k=2)
+            # stores.remove(match[0])
+            # stores.remove(match[1])
+            new_population += self.crossover(match)
+
+    def crossover(self, stores):
+        index = random.randint(1, len(stores[0]))
+        sons = []
+        sons[0] = stores[0][:index] + stores[1][index:]
+        sons[1] = stores[1][:index] + stores[0][index:]
+        return sons
+
+    def score(self, store):
         return sum([compute_distance(order_to_points(i, store)) for i in self.orders])
 
 
@@ -30,9 +59,7 @@ o_len = 8
 store_size = (1, 1)  # Not to big or memory will collapse!
 store = generate_indexed_store(*store_size)
 orders = [create_order(store, random.randint(1, o_len)) for i in range(o_cant)]
-print(orders)
 orders = [sort_order(i, store) for i in orders]
-print(orders)
 
 
 gs = GeneticStore((1, 1), orders)
